cf91dc11df4f283a3893c5942caaa13e2fdfd817
Postmodern <postmodern.mod3@gmail.com>
2012-03-15 18:27:54 -0700

    Added a spec for URI::Generic.
    * Removed the URI::HTTPS spec.
    * Require 'uri/query_params/extensions/generic'.

--- spec/extensions/generic_spec.rb
+++ spec/extensions/generic_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require 'query_params_mixin_examples'
 require 'uri/query_params/extensions/generic'
 
-describe URI::HTTP do
-  let(:uri) { URI('http://www.example.com/page.php') }
+describe URI::Generic do
+  let(:uri) { URI('/page.php') }
 
   it_should_behave_like "URI::QueryParams::Mixin"
 end
--- spec/extensions/https_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'query_params_mixin_examples'
-require 'uri/query_params/extensions/http'
-
-describe URI::HTTPS do
-  let(:uri) { URI('https://www.example.com/page.php') }
-
-  it_should_behave_like "URI::QueryParams::Mixin"
-end
