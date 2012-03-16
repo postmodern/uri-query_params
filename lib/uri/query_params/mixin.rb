abfa8340bff5d45008c331f9a5d9c396c9240bcb
Postmodern <postmodern.mod3@gmail.com>
2012-03-15 18:34:06 -0700

    Use self.included to re-alias core methods in URI::Generic.
    * Moved URI::Generic specific methods (path_query) out of the Mixin.

--- lib/uri/query_params/mixin.rb
+++ lib/uri/query_params/mixin.rb
@@ -10,6 +10,54 @@ module URI
       # Allows setting the query_params.
       attr_writer :query_params
 
+      def self.included(base)
+        base.module_eval do
+          alias raw_query query
+
+          #
+          # The raw query-string.
+          #
+          # @return [String, nil]
+          #   The raw query-string.
+          #
+          # @see QueryParams.dump
+          #
+          # @since 0.5.2
+          #
+          def query
+            if @query_params
+              URI::QueryParams.dump(@query_params)
+            else
+              raw_query
+            end
+          end
+
+          alias raw_query= query=
+
+          #
+          # Sets the query string and updates query_params.
+          #
+          # @param [String] query_str
+          #   The new URI query string to use.
+          #
+          # @return [String]
+          #   The new URI query string.
+          #
+          # @example
+          #   url.query = 'a=1&b=2'
+          #   # => "a=1&b=2"
+          #
+          # @see QueryParams.parse
+          #
+          def query=(new_query)
+            new_query = (self.raw_query = new_query)
+
+            parse_query_params! if @query_params
+            return new_query
+          end
+        end
+      end
+
       #
       # Deep-copies the {#query_params} from another URL.
       #
@@ -24,46 +72,6 @@ module URI
         end
 
         super(url)
-      end
-
-      #
-      # The raw query-string.
-      #
-      # @return [String, nil]
-      #   The raw query-string.
-      #
-      # @see QueryParams.dump
-      #
-      # @since 0.5.2
-      #
-      def query
-        if @query_params
-          QueryParams.dump(@query_params)
-        else
-          super
-        end
-      end
-
-      #
-      # Sets the query string and updates query_params.
-      #
-      # @param [String] query_str
-      #   The new URI query string to use.
-      #
-      # @return [String]
-      #   The new URI query string.
-      #
-      # @example
-      #   url.query = 'a=1&b=2'
-      #   # => "a=1&b=2"
-      #
-      # @see QueryParams.parse
-      #
-      def query=(raw_query)
-        new_query = super(raw_query)
-
-        parse_query_params! if @query_params
-        return new_query
       end
 
       #
@@ -111,26 +119,9 @@ module URI
       # @since 0.5.2
       #
       def parse_query_params!
-        @query_params = QueryParams.parse(@query)
+        @query_params = URI::QueryParams.parse(@query)
       end
 
-      private
-
-      def path_query
-        if @query_params
-          str = @path
-
-          unless @query_params.empty?
-            str += '?' + QueryParams.dump(@query_params)
-          end
-
-          str
-        else
-          # do not rebuild the path-query, if the query_params have not
-          # been parsed yet
-          super
-        end
-      end
     end
   end
 end
