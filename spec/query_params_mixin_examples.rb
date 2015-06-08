a054797dfbcf262d166225eedc766ede038406c1
Postmodern <postmodern.mod3@gmail.com>
2015-06-08 10:36:22 -0700

    Upgrade to rspec 3.x.

--- spec/query_params_mixin_examples.rb
+++ spec/query_params_mixin_examples.rb
@@ -11,61 +11,61 @@ shared_examples_for "URI::QueryParams::Mixin" do
   before(:each) { uri.query = query }
 
   it "should include QueryParams" do
-    subject.class.should include(URI::QueryParams::Mixin)
+    expect(subject.class).to include(URI::QueryParams::Mixin)
   end
 
   it "should still provide access to #query" do
-    uri.query.should == query
+    expect(uri.query).to eq(query)
   end
 
   it "should provide #query_params" do
-    should respond_to(:query_params)
+    is_expected.to respond_to(:query_params)
   end
 
   it "should update #query_params after #query is set" do
     subject.query = 'u=2'
-    subject.query_params['u'].should == '2'
+    expect(subject.query_params['u']).to eq('2')
   end
 
   it "should dump out the #query_params when accessing #query" do
     subject.query_params = {'u' => '3'}
-    subject.query.should == 'u=3'
+    expect(subject.query).to eq('u=3')
   end
 
   it "should properly escape query param values" do
     subject.query_params = {'x' => '1&2', 'y' => 'one=two', 'z' => '?'}
 
-    subject.to_s.match(/\?(.+)$/)[1].should == "x=1%262&y=one%3Dtwo&z=%3F"
+    expect(subject.to_s.match(/\?(.+)$/)[1]).to eq("x=1%262&y=one%3Dtwo&z=%3F")
   end
 
   it "should deep-copy the query_params Hash" do
     original = subject.query_params.object_id
 
-    subject.dup.query_params.object_id.should_not == original
-    subject.clone.query_params.object_id.should_not == original
+    expect(subject.dup.query_params.object_id).not_to eq(original)
+    expect(subject.clone.query_params.object_id).not_to eq(original)
   end
 
   describe "#query_params" do
     subject { uri.query_params }
 
     it "should be a Hash" do
-      subject.class.should == Hash
+      expect(subject.class).to eq(Hash)
     end
 
     it "should contain params" do
-      should_not be_empty
+      is_expected.not_to be_empty
     end
 
     it "can contain single-word params" do
-      subject['x'].should == '1'
+      expect(subject['x']).to eq('1')
     end
 
     it "can contain multi-word params" do
-      subject['y'].should == 'one two'
+      expect(subject['y']).to eq('one two')
     end
 
     it "can contain empty params" do
-      subject['z'].should be_empty
+      expect(subject['z']).to be_empty
     end
   end
 end
--- spec/query_params_spec.rb
+++ spec/query_params_spec.rb
@@ -6,41 +6,41 @@ require 'uri'
 describe URI::QueryParams do
   describe "parse" do
     it "should not parse an empty String" do
-      subject.parse('').should be_empty
+      expect(subject.parse('')).to be_empty
     end
 
     it "should not parse empty params" do
-      subject.parse('a&&&b').should == {'a' => '', 'b' => ''}
+      expect(subject.parse('a&&&b')).to eq({'a' => '', 'b' => ''})
     end
 
     it "should parse a single query param name" do
-      subject.parse('x').should have_key('x')
+      expect(subject.parse('x')).to have_key('x')
     end
 
     it "should parse a query param with an empty value" do
       query_params = subject.parse('x=')
 
-      query_params.should have_key('x')
-      query_params['x'].should be_empty
+      expect(query_params).to have_key('x')
+      expect(query_params['x']).to be_empty
     end
 
     it "should URI decode query param values" do
       query_params = subject.parse('x=1%202')
 
-      query_params['x'].should == '1 2'
+      expect(query_params['x']).to eq('1 2')
     end
 
     it "should ignore multiple '=' characters in query param values" do
       query_params = subject.parse('x=1=2')
 
-      query_params['x'].should == '1=2'
+      expect(query_params['x']).to eq('1=2')
     end
 
     it "should parse multiple query params" do
       query_params = subject.parse('x=1&y=2')
 
-      query_params['x'].should == '1'
-      query_params['y'].should == '2'
+      expect(query_params['x']).to eq('1')
+      expect(query_params['y']).to eq('2')
     end
 
     context "when given a block" do
@@ -51,7 +51,7 @@ describe URI::QueryParams do
           params << [name, value]
         end
 
-        params.should == [['z', '1'], ['z', '2'], ['z', '3']]
+        expect(params).to eq([['z', '1'], ['z', '2'], ['z', '3']])
       end
 
       it "should yield the query params in the order they are parsed" do
@@ -61,38 +61,38 @@ describe URI::QueryParams do
           params << [name, value]
         end
 
-        params.should == [['z', '1'], ['y', '2'], ['x', '3']]
+        expect(params).to eq([['z', '1'], ['y', '2'], ['x', '3']])
       end
     end
   end
 
   describe "dump" do
     it "should not dump an empty Hash" do
-      subject.dump({}).should be_empty
+      expect(subject.dump({})).to be_empty
     end
 
     it "should dump query params with no values" do
-      subject.dump({'x' => nil}).should == 'x='
+      expect(subject.dump({'x' => nil})).to eq('x=')
     end
 
     it "should dump query params with empty values" do
-      subject.dump({'x' => ''}).should == 'x='
+      expect(subject.dump({'x' => ''})).to eq('x=')
     end
 
     it "should dump query params with true values" do
-      subject.dump({'x' => true}).should == 'x=active'
+      expect(subject.dump({'x' => true})).to eq('x=active')
     end
 
     it "should dump query params with non String values" do
-      subject.dump({'x' => 1}).should == 'x=1'
+      expect(subject.dump({'x' => 1})).to eq('x=1')
     end
 
     it "should dump query params with Array values" do
-      subject.dump({'x' => [1,2]}).should == 'x=1%202'
+      expect(subject.dump({'x' => [1,2]})).to eq('x=1%202')
     end
 
     it "should dump multiple query params" do
-      subject.dump({'x' => '1', 'y' => '2'}).should == 'x=1&y=2'
+      expect(subject.dump({'x' => '1', 'y' => '2'})).to eq('x=1&y=2')
     end
   end
 end
