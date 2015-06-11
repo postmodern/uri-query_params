7b12e027b87e5e0f2b5adb699dbb3322a5e671cd
Postmodern <postmodern.mod3@gmail.com>
2015-06-11 11:41:34 -0700

    Added more spec structure.

--- spec/query_params_mixin_examples.rb
+++ spec/query_params_mixin_examples.rb
@@ -10,39 +10,48 @@ shared_examples_for "URI::QueryParams::Mixin" do
 
   before(:each) { uri.query = query }
 
-  it "should include QueryParams" do
-    expect(subject.class).to include(URI::QueryParams::Mixin)
+  context "when included" do
+    it "should include QueryParams::Mixin" do
+      expect(subject.class).to include(URI::QueryParams::Mixin)
+    end
+
+    it "should still provide access to #query" do
+      expect(uri.query).to eq(query)
+    end
+
+    it "should provide #query_params" do
+      is_expected.to respond_to(:query_params)
+    end
   end
 
-  it "should still provide access to #query" do
-    expect(uri.query).to eq(query)
+  context "when copied" do
+    it "should deep-copy the query_params Hash" do
+      original = subject.query_params.object_id
+
+      expect(subject.dup.query_params.object_id).not_to eq(original)
+      expect(subject.clone.query_params.object_id).not_to eq(original)
+    end
   end
 
-  it "should provide #query_params" do
-    is_expected.to respond_to(:query_params)
+  describe "#query=" do
+    it "should update #query_params after #query is set" do
+      subject.query = 'u=2'
+
+      expect(subject.query_params['u']).to eq('2')
+    end
   end
 
-  it "should update #query_params after #query is set" do
-    subject.query = 'u=2'
-    expect(subject.query_params['u']).to eq('2')
-  end
+  describe "#query" do
+    it "should dump out the #query_params when accessing #query" do
+      subject.query_params = {'u' => '3'}
+      expect(subject.query).to eq('u=3')
+    end
 
-  it "should dump out the #query_params when accessing #query" do
-    subject.query_params = {'u' => '3'}
-    expect(subject.query).to eq('u=3')
-  end
+    it "should properly escape query param values" do
+      subject.query_params = {'x' => '1&2', 'y' => 'one=two', 'z' => '?'}
 
-  it "should properly escape query param values" do
-    subject.query_params = {'x' => '1&2', 'y' => 'one=two', 'z' => '?'}
-
-    expect(subject.query).to eq("x=1%262&y=one%3Dtwo&z=%3F")
-  end
-
-  it "should deep-copy the query_params Hash" do
-    original = subject.query_params.object_id
-
-    expect(subject.dup.query_params.object_id).not_to eq(original)
-    expect(subject.clone.query_params.object_id).not_to eq(original)
+      expect(subject.query).to eq("x=1%262&y=one%3Dtwo&z=%3F")
+    end
   end
 
   describe "#query_params" do
