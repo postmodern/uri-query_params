7dbd2c5f3deaec9e28caf8ac89e88d31d5580853
Postmodern <postmodern.mod3@gmail.com>
2022-10-21 17:22:54 -0700

    Updated `URI::Generic#to_s` to ruby-3.0.0.
    * https://github.com/ruby/ruby/blob/v3_0_0/lib/uri/generic.rb#L1330-L1368

--- lib/uri/query_params/core_ext/uri/generic.rb
+++ lib/uri/query_params/core_ext/uri/generic.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'uri/query_params/mixin'
 
 require 'uri/generic'
@@ -11,44 +13,44 @@ module URI
     # Constructs String from URI
     #
     # @note
-    #   This is the `URI::Generic#to_s` method from Ruby 2.2.0, with the minor
+    #   This is the `URI::Generic#to_s` method from Ruby 3.0.0, with the minor
     #   modification of calling the `query` method overrode by
     #   {URI::QueryParams::Mixin}, instead of `@query`.
     #
-    # @see https://github.com/ruby/ruby/blob/v2_2_0/lib/uri/generic.rb#L1338-L1376
+    # @see https://github.com/ruby/ruby/blob/v3_0.0/lib/uri/generic.rb#L1330-L1368
     #
     def to_s
-      str = ''
+      str = ''.dup
       if @scheme
         str << @scheme
-        str << ':'.freeze
+        str << ':'
       end
 
       if @opaque
         str << @opaque
       else
-        if @host
-          str << '//'.freeze
+        if @host || %w[file postgres].include?(@scheme)
+          str << '//'
         end
         if self.userinfo
           str << self.userinfo
-          str << '@'.freeze
+          str << '@'
         end
         if @host
           str << @host
         end
         if @port && @port != self.default_port
-          str << ':'.freeze
+          str << ':'
           str << @port.to_s
         end
         str << @path
-        if query
-          str << '?'.freeze
+        if (query = self.query)
+          str << '?'
           str << query
         end
       end
       if @fragment
-        str << '#'.freeze
+        str << '#'
         str << @fragment
       end
       str
